87ade6ca60cfba08cc3a7287d62682d7b3193803
Sobirov3636 <sobirov3636@gmail.com>
2024-07-09 09:14:31 +0500

    feat: connect mongoDb database

--- apps/gocar-api/src/database/database.module.ts
+++ apps/gocar-api/src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
-export class DatabaseModule {}
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+	exports: [MongooseModule],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(
+				`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`,
+			);
+		} else {
+			console.log('DB is not connected!');
+		}
+	}
+}
