16176920baa8cb144678de11d6dfbe6f477c459e
Sobirov3636 <sobirov3636@gmail.com>
2024-07-10 08:52:23 +0500

    feat: create components related modules

--- apps/gocar-api/src/components/components.module.ts
+++ apps/gocar-api/src/components/components.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-	imports: [MemberModule, PropertyModule],
+	imports: [
+		MemberModule,
+		AuthModule,
+		PropertyModule,
+		BoardArticleModule,
+		CommentModule,
+		LikeModule,
+		ViewModule,
+		FollowModule,
+	],
 })
 export class ComponentsModule {}
