5563c628c7109bbc671403c93702cf78c22a0378
Sobirov3636 <sobirov3636@gmail.com>
2024-07-10 09:29:46 +0500

    feat: create member schema model

--- apps/gocar-api/src/components/member/member.module.ts
+++ apps/gocar-api/src/components/member/member.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
 	providers: [MemberResolver, MemberService],
 })
 export class MemberModule {}
--- apps/gocar-api/src/components/member/member.service.ts
+++ apps/gocar-api/src/components/member/member.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MemberService {
+	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
 	public async signup(): Promise<string> {
 		return 'signup executed!';
 	}
