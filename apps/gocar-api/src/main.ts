ef38654c79542d37862e4e775bd144e7879e51b5
Sobirov3636 <sobirov3636@gmail.com>
2024-07-10 18:41:44 +0500

    feat: standard integration of loggin interceptor

--- apps/gocar-api/src/main.ts
+++ apps/gocar-api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new LoggingInterceptor());
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
