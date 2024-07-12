e406dc227d43c3643f96c3f7f1eac10b4dedbaeb
Sobirov3636 <sobirov3636@gmail.com>
2024-07-12 05:52:54 +0500

    fix: modify upload images configuration via graphqlUploadExpress

--- apps/gocar-api/src/main.ts
+++ apps/gocar-api/src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress } from 'graphql-upload';
+import * as express from 'express';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
 	app.useGlobalInterceptors(new LoggingInterceptor());
+	app.enableCors({ origin: true, credentials: true });
+
+	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFile: 10 }));
+	app.use('/uploads', express.static('./uploads'));
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
