6c121482d2cc8a42aa832a86c499baca88c6d193
Sobirov3636 <sobirov3636@gmail.com>
2024-07-16 22:31:07 +0500

    feat: develop createBoardArticle GraphQL api

--- apps/gocar-api/src/components/board-article/board-article.resolver.ts
+++ apps/gocar-api/src/components/board-article/board-article.resolver.ts
@@ -1,7 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { BoardArticleService } from './board-article.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class BoardArticleResolver {
 	constructor(private readonly boardArticleService: BoardArticleService) {}
+
+	// CREATE BOARD ARTICLE
+	@UseGuards(AuthGuard)
+	@Mutation(() => BoardArticle)
+	public async createBoardArticle(
+		@Args('input') input: BoardArticleInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Mutation createBoardArticle');
+		return await this.boardArticleService.createBoardArticle(memberId, input);
+	}
 }
--- apps/gocar-api/src/components/board-article/board-article.service.ts
+++ apps/gocar-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Model, ObjectId } from 'mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
 import { InjectModel } from '@nestjs/mongoose';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class BoardArticleService {
@@ -12,4 +14,17 @@ export class BoardArticleService {
 		private memberService: MemberService,
 		private viewService: ViewService,
 	) {}
+
+	// CREATE BOARD ARTICLE
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsEditor({ _id: result.memberId, targetKey: 'memberArticles', modifier: 1 });
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }
