8052130cf2cf6b69754461585fd3dab158f5c7be
Sobirov3636 <sobirov3636@gmail.com>
2024-07-17 16:56:59 +0500

    feat: develop removeCommentByAdmin GraphQL api

--- apps/gocar-api/src/components/comment/comment.resolver.ts
+++ apps/gocar-api/src/components/comment/comment.resolver.ts
@@ -9,6 +9,9 @@ import { ObjectId } from 'mongoose';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { WithoutGuard } from '../auth/guards/without.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -47,5 +50,17 @@ export class CommentResolver {
 		console.log('Query: getComments');
 		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
 		return await this.commentService.getComments(memberId, input);
+	}
+
+	/** ADMIN **/
+
+	// REMOVE COMMENT BY ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Comment)
+	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+		console.log('Mutation: removeCommentByAdmin');
+		const commentId = shapeIntoMongoObjectId(input);
+		return await this.commentService.removeCommentByAdmin(commentId);
 	}
 }
--- apps/gocar-api/src/components/comment/comment.service.ts
+++ apps/gocar-api/src/components/comment/comment.service.ts
@@ -104,4 +104,14 @@ export class CommentService {
 
 		return result[0];
 	}
+
+	/** ADMIN **/
+
+	// REMOVE COMMENT BY ADMIN
+	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input).exec();
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
+	}
 }
